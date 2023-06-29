e9eaca432e98705dac541e9f6447fa843d009c04
Michael Ehrlichman <mpe@slac.stanford.edu>
2023-06-29 14:14:48 -0700

    dev

--- sls_simple/element_list/element_list.f90
+++ sls_simple/element_list/element_list.f90
@@ -14,7 +14,7 @@ call bmad_parser(lat_file, lat)
 open(45,file='element_list.dat')
 do i=0,lat%n_ele_track
   if((lat%ele(i)%name(1:1) == 'Q' .or. lat%ele(i)%name(1:1) == 'B') .or. lat%ele(i)%name(1:1) == 'S') then
-    write(45,'(i7,f14.6,f15.6,a,a,a,i)') i, lat%ele(i)%s, lat%ele(i)%value(l$), '    ', lat%ele(i)%name, lat%ele(i)%alias, lat%ele(i)%slave_status
+    write(45,'(i7,f14.6,f15.6,a,a,a,i3)') i, lat%ele(i)%s, lat%ele(i)%value(l$), '    ', lat%ele(i)%name, lat%ele(i)%alias, lat%ele(i)%slave_status
   endif
 enddo
 
@@ -23,13 +23,13 @@ write(45,*) "#"
 write(45,*) "# Super Elements"
 write(45,*) "#"
 do i=lat%n_ele_track+1, lat%n_ele_max
-  write(45,'(i7,f12.4,f13.4,a,a,i)') i, lat%ele(i)%s, lat%ele(i)%value(l$), '    ', lat%ele(i)%name, lat%ele(i)%slave_status
+  write(45,'(i7,f12.4,f13.4,a,a,i3)') i, lat%ele(i)%s, lat%ele(i)%value(l$), '    ', lat%ele(i)%name, lat%ele(i)%slave_status
 enddo
 close(45)
 
 open(45,file='element_list.all')
 do i=0,lat%n_ele_track
-  write(45,'(i7,f14.6,f15.6,a,a,a,i)') i, lat%ele(i)%s, lat%ele(i)%value(l$), '    ', lat%ele(i)%name, lat%ele(i)%alias, lat%ele(i)%slave_status
+  write(45,'(i7,f14.6,f15.6,a,a,a,i3)') i, lat%ele(i)%s, lat%ele(i)%value(l$), '    ', lat%ele(i)%name, lat%ele(i)%alias, lat%ele(i)%slave_status
 enddo
 close(45)
 
