b5a374b47057220baad8752ad18b4cd42b64ad10
Michael Ehrlichman <mpe@slac.stanford.edu>
2024-09-27 14:58:53 -0700

    added omp

--- sls_simple/long_phase_space/long_phase_space.f90
+++ sls_simple/long_phase_space/long_phase_space.f90
@@ -1,21 +1,22 @@
 program long_phase_space_program
 
 use bmad
-!$use omp_lib
+use omp_lib
 
 implicit none
 
 character lat_file*200
 
 type(lat_struct) lat
+type(lat_struct), allocatable :: omp_lat(:)
 type(coord_struct), allocatable :: orb(:)
 type(coord_struct), allocatable :: co(:)
 type(coord_struct), allocatable :: tbt(:)
 
 real(rp), allocatable :: data(:,:,:,:)
 
-integer h, i, j, k, l
-integer Np, Nturns
+integer h, i, j, k, l, m
+integer Np_a, Np_b, Nturns
 integer track_state
 integer plane
 integer status
@@ -28,15 +29,20 @@ real(rp) d_pt, z_pt
 
 logical printed
 
-Np = 100
-zi = -0.01
-zf =  0.01
-di = -0.02
-df =  0.02
-Nturns = 270
+integer omp_n, omp_i
+logical first
+integer n_ele_track
 
-dz = (zf-zi)/(Np-1)
-dd = (df-di)/(Np-1)
+Np_a = 200
+Np_b = 200
+zi = -0.10
+zf =  0.10
+di = -0.04
+df =  0.08
+Nturns = 500
+
+dz = (zf-zi)/(Np_a-1)
+dd = (df-di)/(Np_b-1)
 
 bmad_com%radiation_damping_on = .false.
 bmad_com%radiation_fluctuations_on = .false.
@@ -46,12 +52,12 @@ call getarg(1, lat_file)
 call bmad_parser(lat_file, lat)
 call set_on_off(rfcavity$, lat, on$)
 
-allocate(co(0:lat%n_ele_track))
+n_ele_track = lat%n_ele_track
+
+allocate(co(0:n_ele_track))
 co(0)%vec = 0.0d0
 call twiss_and_track(lat,co,status)
 
-allocate(orb(0:lat%n_ele_track))
-allocate(tbt(Nturns))
 
 open(45,file='phase_space_z.out')
 write(45,'(3a6,3a14)') "#", "i", "j", "z0", "p0", 'alive'
@@ -59,23 +65,44 @@ write(45,'(3a6,3a14)') "#", "i", "j", "z0", "p0", 'alive'
 open(47,file='dead_or_alive.out')
 write(47,'(3a6,3a14)') "#", "i", "j", "z0", "p0", 'alive'
 
-allocate(data(Np,Np,Nturns,7))
+allocate(data(Np_a,Np_b,Nturns,7))
 
-do i=1,Np
-  do j=1,Np
-    z_pt = zi + dz*(j-1)
-    d_pt = di + dd*(i-1)
+omp_n = omp_get_max_threads()
+allocate(omp_lat(omp_n))
+do i=1,omp_n
+  omp_lat(i) = lat
+enddo
+write(*,*) 'omp_get_max_threads(): ', omp_n
+
+first = .true.
+
+!$OMP PARALLEL DO &
+!$OMP DEFAULT(SHARED), &
+!$OMP FIRSTPRIVATE(first), &
+!$OMP PRIVATE(i,j,k,l,z_pt,d_pt,nok,track_state,orb,tbt,printed,alive,omp_i)
+do m=0, Np_a*Np_b+1
+  omp_i = omp_get_thread_num()+1
+  if (first) then
+    allocate(orb(0:n_ele_track))
+    allocate(tbt(Nturns))
+    first = .false.
+  endif
+  i = m/Np_b + 1
+  j = mod(m,Np_b) + 1
+  ! effectively nexted i=1,Np_a, j=1,Np_b
+    z_pt = zi + dz * (i-1)
+    d_pt = di + dd * (j-1)
     orb(0) = co(0)
     orb(0)%vec(5) = orb(0)%vec(5) + z_pt
     orb(0)%vec(6) = orb(0)%vec(6) + d_pt
-    write(*,'(a,6f12.7)') "Tracking: ", orb(0)%vec
+    write(*,'(a,i3,a,6f12.7)') "thread ", omp_i, " is tracking: ", orb(0)%vec
     nok = 0
     do k=1,Nturns
-      call track_all(lat,orb,track_state=track_state)
+      call track_all(omp_lat(omp_i),orb,track_state=track_state)
       if( track_state .eq. moving_forward$ ) then
         nok = nok + 1
         tbt(k) = orb(0)
-        orb(0) = orb(lat%n_ele_track)
+        orb(0) = orb(n_ele_track)
       else
         write(*,'(a,2i8)') "Particle lost at (turn, ele): ", k, track_state
         exit
@@ -102,16 +129,17 @@ do i=1,Np
       write(45,*)
       write(45,*)
     endif
-  enddo
+  !effective do loop
 enddo
+!$OMP END PARALLEL DO
 close(45)
 close(47)
 
 open(46,file='phase_space_z_transpose.out')
 write(46,'(4a6,9a14)') "#", "i", "j", "turn", "z0", "p0", "x", "px", "y", "py", "z", "pz", 'alive'
 do k=1,Nturns
-  do i=1,Np
-    do j=1,Np
+  do i=1,Np_a
+    do j=1,Np_b
       z_pt = zi + dz*(j-1)
       d_pt = di + dd*(i-1)
       write(46,'(a6,3i6,2f14.5,6es15.4,i6)') '', i, j, k, z_pt, d_pt, data(i,j,k,1:6), int(data(i,j,k,7))
