0e064ddbf6d388cb7631b8a98dd1bd71884ea3b8
Michael P. Ehrlichman <mpe@slac.stanford.edu>
2025-06-25 11:21:24 -0700

    dev branch for icer

--- sls_simple/long_phase_space/long_phase_space.f90
+++ sls_simple/long_phase_space/long_phase_space.f90
@@ -33,13 +33,13 @@ integer omp_n, omp_i
 logical first
 integer n_ele_track
 
-Np_a = 200
-Np_b = 200
-zi = -0.10
-zf =  0.10
-di = -0.04
-df =  0.08
-Nturns = 500
+Np_a = 400
+Np_b = 400
+zi = -0.50
+zf =  0.50
+di = -0.08
+df =  0.04
+Nturns = 750
 
 dz = (zf-zi)/(Np_a-1)
 dd = (df-di)/(Np_b-1)
@@ -77,10 +77,11 @@ write(*,*) 'omp_get_max_threads(): ', omp_n
 first = .true.
 
 !$OMP PARALLEL DO &
+!$OMP SCHEDULE(dynamic,1), &
 !$OMP DEFAULT(SHARED), &
 !$OMP FIRSTPRIVATE(first), &
 !$OMP PRIVATE(i,j,k,l,z_pt,d_pt,nok,track_state,orb,tbt,printed,alive,omp_i)
-do m=0, Np_a*Np_b+1
+do m=0, Np_a*Np_b-1
   omp_i = omp_get_thread_num()+1
   if (first) then
     allocate(orb(0:n_ele_track))
@@ -135,20 +136,20 @@ enddo
 close(45)
 close(47)
 
-open(46,file='phase_space_z_transpose.out')
-write(46,'(4a6,9a14)') "#", "i", "j", "turn", "z0", "p0", "x", "px", "y", "py", "z", "pz", 'alive'
-do k=1,Nturns
-  do i=1,Np_a
-    do j=1,Np_b
-      z_pt = zi + dz*(j-1)
-      d_pt = di + dd*(i-1)
-      write(46,'(a6,3i6,2f14.5,6es15.4,i6)') '', i, j, k, z_pt, d_pt, data(i,j,k,1:6), int(data(i,j,k,7))
-    enddo
-  enddo
-  write(46,*)
-  write(46,*)
-enddo
-close(46)
+!open(46,file='phase_space_z_transpose.out')
+!write(46,'(4a6,9a14)') "#", "i", "j", "turn", "z0", "p0", "x", "px", "y", "py", "z", "pz", 'alive'
+!do k=1,Nturns
+!  do i=1,Np_a
+!    do j=1,Np_b
+!      z_pt = zi + dz*(j-1)
+!      d_pt = di + dd*(i-1)
+!      write(46,'(a6,3i6,2f14.5,6es15.4,i6)') '', i, j, k, z_pt, d_pt, data(i,j,k,1:6), int(data(i,j,k,7))
+!    enddo
+!  enddo
+!  write(46,*)
+!  write(46,*)
+!enddo
+!close(46)
 
 deallocate(data)
 
